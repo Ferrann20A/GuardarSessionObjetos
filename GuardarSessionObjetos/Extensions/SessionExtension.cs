44a688ab6dcdc294d56bc027536bce8d0fb3b024
Ferrann20A <alumno.398783@ies-azarquiel.es>
2024-04-01 18:57:57 +0200

    Add project files.

---
+++ GuardarSessionObjetos/Extensions/SessionExtension.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace GuardarSessionObjetos.Extensions
+{
+    public static class SessionExtension
+    {
+        //MÉTODO PARA RECUPERAR CUALQUIER OBJETO
+        public static T GetObject<T>(this ISession session , string key)
+        {
+            string json = session.GetString(key);
+            if(json == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                T data = JsonConvert.DeserializeObject<T>(json);
+                return data;
+            }
+        }
+
+        //MÉTODO PARA ALMACENAR CUALQUIER OBJETO
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            string data = JsonConvert.SerializeObject(value);
+            session.SetString(key, data);
+        }
+    }
+}
---
+++ GuardarSessionObjetos/Models/Coche.cs
@@ -0,0 +1,9 @@
+﻿namespace GuardarSessionObjetos.Models
+{
+    public class Coche
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Precio { get; set; }
+    }
+}
